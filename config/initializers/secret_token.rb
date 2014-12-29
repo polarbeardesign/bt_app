9e964b7f747074993f3f9b18a9ea468adfc742eb
Jim Ivanoff <jim@polarbeardesign.net>
2014-12-29 13:13:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BtApp::Application.config.secret_token = 'fa1b3a9322acf1d39e51c24a4999725bd6ac54bc6f883b4a3f7d522122f7f2eaeece68f592c4a151103d5309eeb8d2b78eb0b4f125eb6f8f179f4a5f206db200'

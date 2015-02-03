c643ab2edc139d071f1bf3b530c173c31dce41b9
Jim Ivanoff <jim@polarbeardesign.net>
2015-02-03 11:47:52 +0100

    updates to add post date icon and fix adding user id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   
-  attr_accessible :title, :content, :published, :user
+  attr_accessible :title, :content, :published, :user_id
   
   scope :reverse, order("posts.created_at DESC")
 
